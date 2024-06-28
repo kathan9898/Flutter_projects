4908f39e74ed0729649f44fb838b333995807813
kathan9898 <patelkathan6868@gmail.com>
2024-06-28 21:11:48 +0530

    Add make group ui.

--- splitwise/lib/home_page.dart
+++ splitwise/lib/home_page.dart
@@ -1,27 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String email;
 
   const HomePage({Key? key, required this.email}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  final TextEditingController _groupNameController = TextEditingController();
+  final TextEditingController _groupDescriptionController = TextEditingController();
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Future<void> _submitGroup() async {
+    final String apiUrl = 'https://yourserver.com/api/submit_group.php';
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      body: {
+        'email': widget.email,
+        'group_name': _groupNameController.text,
+        'group_description': _groupDescriptionController.text,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['status'] == 'success') {
+        final String username = data['username'];
+        print('Username: $username');
+        // Do something with the username, e.g., show a success message
+      } else {
+        print('Error: ${data['message']}');
+      }
+    } else {
+      print('Server error: ${response.statusCode}');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true, // Extend body behind the app bar
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.transparent, // Make app bar transparent
-        elevation: 0, // Remove elevation
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
             child: Row(
               children: [
-                Icon(Icons.person, color: Colors.white), // Adjust icon color
+                Icon(Icons.person, color: Colors.white),
                 SizedBox(width: 8),
                 Text(
-                  'User: $email',
-                  style: TextStyle(color: Colors.white), // Adjust text color
+                  'User: ${widget.email}',
+                  style: TextStyle(color: Colors.white),
                 ),
               ],
             ),
@@ -30,88 +72,147 @@ class HomePage extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          // Background Image
           Image.asset(
-            'assets/home_page.jpg', // Adjust path as per your project structure
+            'assets/home_page.jpg',
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
           ),
-          // Transparent Title Box
           Positioned(
             top: 0,
             left: 0,
             right: 0,
-            height: MediaQuery.of(context).size.height * 0.3, // 30% of screen height
+            height: MediaQuery.of(context).size.height * 0.3,
             child: Container(
               padding: EdgeInsets.all(16.0),
-              color: Colors.black.withOpacity(0.4), // Adjust opacity as needed
+              color: Colors.black.withOpacity(0.4),
               child: Center(
                 child: Text(
                   'Welcome to SplitWise!',
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white, // Text color
+                    color: Colors.white,
                   ),
                 ),
               ),
             ),
           ),
-          // Tabs Row
           Positioned(
             top: MediaQuery.of(context).size.height * 0.3,
             left: 0,
             right: 0,
             child: Container(
-              color: Colors.pink[200]?.withOpacity(0.8), // Conditional access
-              padding: EdgeInsets.all(10.0),
+              color: Colors.black?.withOpacity(0.5),
+              padding: EdgeInsets.all(15.0),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
-                    TabItem(text: 'Group'), // Custom TabItem widget
-                    TabItem(text: 'Make Group'), // Custom TabItem widget
-                    TabItem(text: 'Manage Groups'), // Custom TabItem widget
+                    TabItem(text: 'Group', index: 0, onTap: _onTabSelected),
+                    TabItem(text: 'Make Group', index: 1, onTap: _onTabSelected),
+                    TabItem(text: 'Manage Groups', index: 2, onTap: _onTabSelected),
                   ],
                 ),
               ),
             ),
           ),
-          // Main Content Area
           Positioned.fill(
             top: MediaQuery.of(context).size.height * 0.40,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Content for Selected Tab',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Logged in as:',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Text(
-                    email,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            child: _buildContent(),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    switch (_selectedIndex) {
+      case 1:
+        return _buildMakeGroupContent();
+      default:
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Content for Selected Tab',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Logged in as:',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey,
+                ),
+              ),
+              Text(
+                widget.email,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        );
+    }
+  }
+
+  Widget _buildMakeGroupContent() {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Card(
+          color: Colors.white.withOpacity(0.8),
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Initiate Your Circle',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 50),
+                TextField(
+                  controller: _groupNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Group Name',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: _groupDescriptionController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Group Description',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _submitGroup,
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -119,14 +220,16 @@ class HomePage extends StatelessWidget {
 
 class TabItem extends StatelessWidget {
   final String text;
+  final int index;
+  final ValueChanged<int> onTap;
 
-  const TabItem({Key? key, required this.text}) : super(key: key);
+  const TabItem({Key? key, required this.text, required this.index, required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        // Handle tap on the tab
+        onTap(index);
       },
       child: Container(
         padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
